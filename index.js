af9e72ce0bed7232f25fbae2ccd4b0b3cd34d357
Caelin Sutch <32991829+caelinsutch@users.noreply.github.com>
2021-04-09 18:29:18 -0700

    Override getDetoxVersion

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-const { UrlUtils } = require('@expo/xdl');
-const cp = require('child_process');
-const fs = require('fs');
-const os = require('os');
-const path = require('path');
-const semver = require('semver');
+const { UrlUtils } = require("@expo/xdl");
+const cp = require("child_process");
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
+const semver = require("semver");
 
-const OSX_APP_PATH = path.join(os.homedir(), 'Library');
-const OSX_LIBRARY_ROOT_PATH = path.join(OSX_APP_PATH, 'ExpoDetoxHook');
+const OSX_APP_PATH = path.join(os.homedir(), "Library");
+const OSX_LIBRARY_ROOT_PATH = path.join(OSX_APP_PATH, "ExpoDetoxHook");
 
-const expoDetoxHookAppPath = path.join(process.cwd(), 'node_modules/expo-detox-hook');
-const expoDetoxHookPackageJsonPath = path.join(expoDetoxHookAppPath, 'package.json');
+const expoDetoxHookAppPath = path.join(
+  process.cwd(),
+  "node_modules/expo-detox-hook"
+);
+const expoDetoxHookPackageJsonPath = path.join(
+  expoDetoxHookAppPath,
+  "package.json"
+);
 
 function getFrameworkPath() {
   const version = require(expoDetoxHookPackageJsonPath).version;
-  let sha1 = cp.execSync(`(echo "${version}" && xcodebuild -version) | shasum | awk '{print $1}'`).toString().trim();
+  let sha1 = cp
+    .execSync(
+      `(echo "${version}" && xcodebuild -version) | shasum | awk '{print $1}'`
+    )
+    .toString()
+    .trim();
   return `${OSX_LIBRARY_ROOT_PATH}/ios/${sha1}/ExpoDetoxHook.framework/ExpoDetoxHook`;
 }
 
@@ -29,7 +40,7 @@ const getAppUrl = async () => {
 const getAppHttpUrl = async () => {
   const httpUrl = await UrlUtils.constructUrlAsync(
     process.cwd(),
-    { urlType: 'http' },
+    { urlType: "http" },
     true
   );
   return httpUrl;
@@ -56,38 +67,44 @@ const init = () => {
   initialized = true;
 
   if (!fs.existsSync(expoDetoxHookPackageJsonPath)) {
-    throw new Error("expo-detox-hook is not installed in this directory. You should declare it in package.json and run `npm install`");
+    throw new Error(
+      "expo-detox-hook is not installed in this directory. You should declare it in package.json and run `npm install`"
+    );
   }
 
   const expoDetoxHookFrameworkPath = getFrameworkPath();
 
   if (!fs.existsSync(expoDetoxHookFrameworkPath)) {
-    throw new Error("expo-detox-hook is not installed in your osx Library. Run `npm install -g expo-detox-cli && expotox clean-framework-cache && expotox build-framework-cache` to fix this.");
+    throw new Error(
+      "expo-detox-hook is not installed in your osx Library. Run `npm install -g expo-detox-cli && expotox clean-framework-cache && expotox build-framework-cache` to fix this."
+    );
   }
 
   detoxVersion = getDetoxVersion();
   oldEnvVar = process.env.SIMCTL_CHILD_DYLD_INSERT_LIBRARIES;
 
-  if (semver.gte(detoxVersion, '9.0.6')) {
+  if (semver.gte(detoxVersion, "9.0.6")) {
     process.env.SIMCTL_CHILD_DYLD_INSERT_LIBRARIES = expoDetoxHookFrameworkPath;
   }
-}
+};
 
 const reloadApp = async (params) => {
-  init();
-
-  const formattedBlacklistArg = await blacklistCmdlineFormat(params && params.urlBlacklist);
+  const formattedBlacklistArg = await blacklistCmdlineFormat(
+    params && params.urlBlacklist
+  );
   const url = await getAppUrl();
   await device.launchApp({
     permissions: params && params.permissions,
     newInstance: true,
     url,
-    sourceApp: 'host.exp.exponent',
-    launchArgs: { EXKernelDisableNuxDefaultsKey: true, detoxURLBlacklistRegex: formattedBlacklistArg },
+    sourceApp: "host.exp.exponent",
+    launchArgs: {
+      EXKernelDisableNuxDefaultsKey: true,
+      detoxURLBlacklistRegex: formattedBlacklistArg,
+    },
   });
 
-
-  if (semver.lt(detoxVersion, '9.0.6')) {
+  if (semver.lt(detoxVersion, "9.0.6")) {
     // we will need to pass in blacklist again before it was supported at init in 9.0.6
     await blacklistLiveReloadUrl(params && params.urlBlacklist);
   } else {
@@ -96,37 +113,47 @@ const reloadApp = async (params) => {
 };
 
 const getDetoxVersion = () => {
-  const detoxPackageJsonPath = path.join(process.cwd(), 'node_modules/detox/package.json');
+  const detoxPackageJsonPath = path.join(
+    process.cwd(),
+    "node_modules/detox/package.json"
+  );
   if (!fs.existsSync(detoxPackageJsonPath)) {
-    throw new Error(`detox is not installed in this directory: ${detoxPackageJsonPath}`);
+    throw new Error(
+      `detox is not installed in this directory: ${detoxPackageJsonPath}`
+    );
   }
 
-  return require(detoxPackageJsonPath).version;
+  // return require(detoxPackageJsonPath).version;
+  return "18.11.0";
 };
 
 function escapeRegExp(str) {
-  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
 }
 
 const getBlacklist = async () => {
   const httpUrl = await getAppHttpUrl();
   const liveReloadUrl = `${httpUrl}/onchange`;
-  const fabricUrl = 'https://e.crashlytics.com/spi/v2/events';
+  const fabricUrl = "https://e.crashlytics.com/spi/v2/events";
   return [liveReloadUrl, fabricUrl];
 };
 
 // returns blacklist arg in format like \\("http://192.168.1.253:19001/onchange","https://e.crashlytics.com/spi/v2/events"\\)
 const blacklistCmdlineFormat = async (userBlacklist) => {
   const expoBlacklist = await getBlacklist();
-  const blacklist = userBlacklist ? expoBlacklist.concat(userBlacklist) : expoBlacklist;
-  const cmdlineFormatBlacklist = blacklist.map(url => `"${url}"`).join(",");
+  const blacklist = userBlacklist
+    ? expoBlacklist.concat(userBlacklist)
+    : expoBlacklist;
+  const cmdlineFormatBlacklist = blacklist.map((url) => `"${url}"`).join(",");
   return `\\(${cmdlineFormatBlacklist}\\)`;
 };
 
 const blacklistLiveReloadUrl = async (userBlacklist) => {
   const expoBlacklist = await getBlacklist();
-  const blacklist = userBlacklist ? expoBlacklist.concat(userBlacklist) : expoBlacklist;
-  const regexBlacklist = blacklist.map(url => escapeRegExp(url));
+  const blacklist = userBlacklist
+    ? expoBlacklist.concat(userBlacklist)
+    : expoBlacklist;
+  const regexBlacklist = blacklist.map((url) => escapeRegExp(url));
   await device.setURLBlacklist(regexBlacklist);
 };
 
